485d789d94e5ec4baaf531e791cf3923219ee607
jbirdjon <jonathanwallis@my.unt.edu>
2019-11-24 15:17:09 -0600

    reorganized some things to make the code prettier

--- WildKarrde/app/src/main/java/com/example/wildkarrde/viewDailyTasks.java
+++ WildKarrde/app/src/main/java/com/example/wildkarrde/viewDailyTasks.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 
 public class viewDailyTasks extends AppCompatActivity {
 
+    private ArrayList<DailyTask> taskList;
+
     private RecyclerView DailyRecyclerView;
     private RecyclerView.Adapter DailyAdapter;
     private RecyclerView.LayoutManager DailyLayoutManager;
@@ -19,11 +21,21 @@ public class viewDailyTasks extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_daily_tasks);
 
-        //get arraylist of items
-        ArrayList<DailyTask> taskList = new ArrayList<>();
+        //created taskList and populate the arraylist
+        createTaskList();
+        //build the recyclerview so we have one!
+        buildDailyRecyclerView();
+
+    }
+
+    public void createTaskList(){
+        taskList = new ArrayList<>();
+
+        //here is where I would get and populate new arraylist to display.
         taskList.add(new DailyTask(R.drawable.ic_not_done, "Take out Trash", "4:45-5:00am"));
         taskList.add(new DailyTask(R.drawable.ic_not_done, "Do HW", "5:00am-9:00pm"));
-
+    }
+    public void buildDailyRecyclerView(){
         DailyRecyclerView = findViewById(R.id.dailyRecyclerView);
         DailyRecyclerView.setHasFixedSize(true);
         DailyLayoutManager = new LinearLayoutManager(this);
@@ -31,10 +43,6 @@ public class viewDailyTasks extends AppCompatActivity {
 
         DailyRecyclerView.setLayoutManager(DailyLayoutManager);
         DailyRecyclerView.setAdapter(DailyAdapter);
-        //need to have recycler view for viewdailytasks
-
-
-        //declare adapter and stuff
     }
 
 }
